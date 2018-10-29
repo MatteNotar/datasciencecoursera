cfbdc2a461df573079c78bc06fef62ee979c3811
Matteo <matteo.notargiacomo@gmail.com>
2018-10-29 18:50:49 -0400

    adding code for assignment

---
+++ ProgrammingAssignment2-code/cachematrix.R
@@ -0,0 +1,32 @@
+
+## makeCacheMatrix instantiates a new vector, with environment having get,set,getCache and setCache methods
+## returns the functions as part of a list
+
+makeCacheMatrix <- function(x = matrix()) {
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setCache <- function(solve) inv <<- solve 
+	getCache <- function() inv
+	list(set = set, get = get, setCache = setCache, getCache = getCache)
+}
+
+
+## cacheSolve tries first to retrieve the value of the inverse from cache
+## if not present in cache, will calcualte inverse with solve function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+	inv <- x$getCache()
+	if(!is.null(inv)) {
+		message("getting the inverse matrix")
+		return(inv)
+	}
+	matrix <- x$get()
+	inv <- solve(matrix, ...)
+	x$setCache(inv)
+	inv
+}
